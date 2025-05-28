990ffb2d37850395cdaeea5ad566ac62aa46c8c9
kakiba <97882386+kotto5@users.noreply.github.com>
2025-05-28 14:33:15 +0000

    Grace.c has implemented

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <fcntl.h>
 #define B(x) x
 B(int main(){ // Hoge
-  char *s = "#include <stdio.h>%c#define B(x) x%cB(int main(){ // Hoge%c  char *s = %c%s%c;%c  printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10);%c})%c";
-  printf(s, 10, 10, 10, 34, s, 34, 10, 10, 10);
+int fd = open("./Grace_kid.c", O_CREAT | O_RDWR);char *s = "#include <stdio.h>%c#include <fcntl.h>%c#define B(x) x%cB(int main(){ // Hoge%cint fd = open(%c./Grace_kid.c%c, O_CREAT | O_RDWR);char *s = %c%s%c;%cdprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, s, 34, 10, 10, 10);%c})%c";
+dprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, s, 34, 10, 10, 10);
 })
