81f0498f518bf0c9176b72741ad42b72e2e1af5f
kakiba <97882386+kotto5@users.noreply.github.com>
2025-05-29 03:09:32 +0000

    refactoring using three macros

--- C/Grace.c
+++ C/Grace.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
-#define B(x) x
-B(int main(){ // Hoge
-int fd = open("./Grace_kid.c", O_CREAT | O_RDWR);char *s = "#include <stdio.h>%c#include <fcntl.h>%c#define B(x) x%cB(int main(){ // Hoge%cint fd = open(%c./Grace_kid.c%c, O_CREAT | O_RDWR);char *s = %c%s%c;%cdprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, s, 34, 10, 10, 10);%c})%c";
-dprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, s, 34, 10, 10, 10);
-})
+#define TOSTRING(X) #X
+#define SOURCE int main() {int fd = open("./Grace_kid.c", 0100 | 02);char *str; str = "#include <stdio.h>%c#include <fcntl.h>%c#define TOSTRING(X) #X%c#define SOURCE %s%c#define FT(SRC) SRC dprintf(fd, str, 10, 10, 10, TOSTRING(SRC), 10, 10, 10);}%cFT(SOURCE)%c";
+#define FT(SRC) SRC dprintf(fd, str, 10, 10, 10, TOSTRING(SRC), 10, 10, 10);}
+FT(SOURCE)
