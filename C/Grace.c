46c27fa0f456b677089c000d62a0d9666a5ee2b2
kakiba <97882386+kotto5@users.noreply.github.com>
2025-05-31 07:40:27 +0000

    update .gitignore

--- C/Grace.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-#define TOSTRING(X) #X
-#define SOURCE int main() {int fd = open("./Grace_kid.c", 0100 | 02);char *str; str = "#include <stdio.h>%c#include <fcntl.h>%c#define TOSTRING(X) #X%c#define SOURCE %s%c#define FT(SRC) SRC dprintf(fd, str, 10, 10, 10, TOSTRING(SRC), 10, 10, 10);}%cFT(SOURCE)%c";
-#define FT(SRC) SRC dprintf(fd, str, 10, 10, 10, TOSTRING(SRC), 10, 10, 10);}
-FT(SOURCE)
