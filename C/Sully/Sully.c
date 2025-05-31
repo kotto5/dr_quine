7beb030f71a349aabf07860bb54842af986d165c
kotto5 <kotto555555@gmail.com>
2025-06-01 01:45:49 +0900

    refactor main function for improved readability and maintainability printf の新たな機能を知った

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -4,11 +4,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #define LAST_CHAR(str)  ((str)[strlen(str) - 3])
-#define FILE_PREFIX 5
 #define GET_PREFIX(ch) (ch == 'y' ? '5' : ch >= '1' && ch <= '5' ? ch - 1 : '\0')
 
-int main(){char prefix = GET_PREFIX(LAST_CHAR(__FILE__));if (prefix == '\0') return 0;
-char filename[8] = "Sully_"; filename[6] = prefix;filename[7] = '\0';char c_file[10] = {0};strcat(c_file, filename);strcat(c_file, ".c");
-int fd = open(c_file, 0100 | 02, 0777);char command[100] = {0};
-char *s = "#include <string.h>%c#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c#define LAST_CHAR(str)  ((str)[strlen(str) - 3])%c#define FILE_PREFIX 5%c#define GET_PREFIX(ch) (ch == 'y' ? '5' : ch >= '1' && ch <= '5' ? ch - 1 : '%c0')%c%cint main(){char prefix = GET_PREFIX(LAST_CHAR(__FILE__));if (prefix == '%c0') return 0;%cchar filename[8] = %cSully_%c;filename[6] = prefix;filename[7] = '%c0';char c_file[10] = {0};strcat(c_file, filename);strcat(c_file, %c.c%c);%cint fd = open(c_file, 0100 | 02, 0777);char command[100] = {0};%cchar *s = %c%s%c;%cdprintf(fd, s, 10,10,10,10,10,10,10,92,10,10, 92,10, 34,34,92,34,34,10, 10,34,s,34,10, 34,34,34,34,34,34,34,34,10);system(strcat(strcat(strcat(strcat(command, %cgcc %c), c_file), %c -o %c), filename));bzero(command, 100);execlp(strcat(strcat(command, %c./%c), filename), strcat(command, %c./%c), filename, NULL);}%c";
-dprintf(fd, s, 10,10,10,10,10,10,10,92,10,10, 92,10, 34,34,92,34,34,10, 10,34,s,34,10, 34,34,34,34,34,34,34,34,10);system(strcat(strcat(strcat(strcat(command, "gcc "), c_file), " -o "), filename));bzero(command, 100);execlp(strcat(strcat(command, "./"), filename), strcat(command, "./"), filename, NULL);}
+int main(){
+    char prefix = GET_PREFIX(LAST_CHAR(__FILE__));
+    if (prefix == '\0') return 0;
+    char filename[16] = {0};
+    sprintf(filename, "Sully_%c", prefix);
+    char c_file[16] = {0};
+    sprintf(c_file, "%s.c", filename);
+    int fd = open(c_file, O_CREAT | O_WRONLY, 0777);
+    if (fd == -1) {perror("error(open):");return 1;}
+    char *s = "#include <string.h>%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c#define LAST_CHAR(str)  ((str)[strlen(str) - 3])%1$c#define GET_PREFIX(ch) (ch == 'y' ? '5' : ch >= '1' && ch <= '5' ? ch - 1 : '%3$c0')%1$c%1$cint main(){%1$c    char prefix = GET_PREFIX(LAST_CHAR(__FILE__));%1$c    if (prefix == '%3$c0') return 0;%1$c    char filename[16] = {0};%1$c    sprintf(filename, %2$cSully_%%c%2$c, prefix);%1$c    char c_file[16] = {0};%1$c    sprintf(c_file, %2$c%%s.c%2$c, filename);%1$c    int fd = open(c_file, O_CREAT | O_WRONLY, 0777);%1$c    if (fd == -1) {perror(%2$cerror(open):%2$c);return 1;}%1$c    char *s = %2$c%4$s%2$c;%1$c    dprintf(fd, s, 10, 34, 92, s);%1$c    char command[100] = {0};%1$c    sprintf(command, %2$cgcc %%s -o %%s%2$c, c_file, filename);%1$c    system(command);%1$c    bzero(command, 100);%1$c    sprintf(command, %2$c./%%s%2$c, filename);%1$c    char *args[] = {command, filename, NULL};%1$c    execvp(command, args);%1$c}%1$c";
+    dprintf(fd, s, 10, 34, 92, s);
+    char command[100] = {0};
+    sprintf(command, "gcc %s -o %s", c_file, filename);
+    system(command);
+    bzero(command, 100);
+    sprintf(command, "./%s", filename);
+    char *args[] = {command, filename, NULL};
+    execvp(command, args);
+}
