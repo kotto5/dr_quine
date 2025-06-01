8d7567212d7a00259a4fa59329c4494301651e3f
kotto5 <kotto555555@gmail.com>
2025-06-01 19:50:49 +0900

    Modify option value for linux OS

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -2,6 +2,6 @@
 #include <fcntl.h>
 /* open's option value is different each OS... */
 #define TOSTRING(X) #X
-#define SOURCE int main() {int fd = open("./Grace_kid.c", 0x00000200 | 0x0002);char *str; str = "#include <stdio.h>%1$c#include <fcntl.h>%1$c/* open's option value is different each OS... */%1$c#define TOSTRING(X) #X%1$c#define SOURCE %2$s%1$c#define FT(SRC) SRC dprintf(fd, str, 10, TOSTRING(SRC));}%1$cFT(SOURCE)%1$c";
+#define SOURCE int main() {int fd = open("./Grace_kid.c", 01 | 100, 0777);char *str; str = "#include <stdio.h>%1$c#include <fcntl.h>%1$c/* open's option value is different each OS... */%1$c#define TOSTRING(X) #X%1$c#define SOURCE %2$s%1$c#define FT(SRC) SRC dprintf(fd, str, 10, TOSTRING(SRC));}%1$cFT(SOURCE)%1$c";
 #define FT(SRC) SRC dprintf(fd, str, 10, TOSTRING(SRC));}
 FT(SOURCE)
